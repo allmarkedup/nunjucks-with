3ac21cfb2bb9e22e9a754c05bc2db69415702c8d
Mark Perkins <mark@allmarkedup.com>
2017-10-23 22:46:07 +0100

    Add missing test file

---
+++ test.js
@@ -0,0 +1,34 @@
+const {expect} = require('chai');
+const nunjucks = require('nunjucks');
+const WithExtension = require('./with');
+
+const MockLoader = nunjucks.Loader.extend({
+  getSource: function(name) {
+    return {
+      src: '{{ text }}',
+      path: name
+    };  
+  }
+});
+const env = new nunjucks.Environment(new MockLoader());
+env.addExtension('WithExtension', new WithExtension());
+
+describe('with tag', function () {
+  it('should isolate scope', function () {
+    const tpl = `{% with {} %}{{ foo }}{% endwith %}`;
+    expect(env.renderString(tpl, {foo: 'bar'})).to.equal('');
+  });
+  it('should set the scope to the provided variable', function () {
+    const tpl = `{% with child %}{{ text }}{% endwith %}`;
+    expect(env.renderString(tpl, {child: {text: 'child'}})).to.equal('child');
+  });
+  it('accepts dynamically provided objects', function () {
+    const tpl = `{% with {text: 'child'} %}{{ text }}{% endwith %}`;
+    expect(env.renderString(tpl, {text: 'parent'})).to.equal('child');
+  });
+  it('works as expected with includes', function () {
+    const tpl = `{% with child %}{% include 'child.tpl' %}{% endwith %}`;
+    expect(env.renderString(tpl, {text: 'parent', child: {text: 'child'}})).to.equal('child');
+  });
+});
+
